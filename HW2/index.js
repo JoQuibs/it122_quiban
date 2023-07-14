df706e404464e1d8e2ab9bb731ed3b6c27316460
Jomar Quiban <104065461+JoQuibs@users.noreply.github.com>
2023-07-13 19:11:28 -0700

    Update index.js

--- HW2/index.js
+++ HW2/index.js
@@ -1,3 +1,27 @@
+const express = require('express');
+const ejs = require('ejs');
 const data = require('./data.js');
 
-console.log(data); // Output the data array
+const app = express();
+const port = 8080;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  const allData = data.getAll();
+  res.render('home', { data: allData });
+});
+
+app.get('/detail', (req, res) => {
+  const itemId = req.query.id;
+  console.log('Requested item ID:', itemId);
+  const { item, key } = data.getItem('key', parseInt(itemId));
+  console.log('Retrieved item:', item);
+  res.render('detail', { item, key });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
