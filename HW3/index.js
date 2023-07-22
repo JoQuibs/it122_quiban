5b1edbeb5c5fb879d021cb02448b6fd681390799
Jomar Quiban <104065461+JoQuibs@users.noreply.github.com>
2023-07-21 17:44:57 -0700

    Update index.js
    HW4 update

--- HW3/index.js
+++ HW3/index.js
@@ -3,30 +3,51 @@ import express from "express";
 import Game from './models/item.js';
 import ejs from 'ejs';
 
+import cors from 'cors';
+
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('./public'));
 app.use(express.json());
+app.use('/api', cors());
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next) => {
-  Game.find({})
-    .lean()
-    .then((game) => {
-      res.render('home', { data: game });
-    })
-    .catch((err) => next(err));
+//HW4 REST API
+// Get all items
+app.get('/api/items', async (req, res) => {
+  const games = await Game.find();
+  res.status(200).json(games);
 });
 
-app.get('/detail', (req, res, next) => {
-    Game.findOne({title:req.query.id}).lean()
-    .then((game) => {
-        console.log(game);
-        res.render('detail', { layout: 'index', game });
-    })
-    .catch(err => next(err));
+// Get a single item
+app.get('/api/items/:id', async (req, res) => {
+  const game = await Game.findOne({ _id: req.params.id });
+  res.status(200).json(game);
+});
 
+// Delete an item
+app.delete('/api/items/:id', async (req, res) => {
+  await Game.deleteOne({ _id: req.params.id });
+  res.status(204).send();
+});
+
+// Add or update an item
+app.post('/api/items', async (req, res) => {
+  const game = new Game(req.body);
+  await game.save();
+  res.status(201).json(game);
+});
+
+//HW3
+app.get('/', async (req, res, next) => {
+  const games = await getGames();
+  res.render('home', { data: games });
+});
+
+app.get('/detail', async (req, res, next) => {
+    const game = await Game.findOne({title:req.query.id});
+    res.render('detail', { layout: 'index', game });
 });
 
 app.use((req, res) => {
@@ -38,3 +59,8 @@ app.use((req, res) => {
 app.listen(app.get('port'), () => {
   console.log('Express started');
 });
+
+async function getGames() {
+  const games = await Game.find();
+  return games;
+}
